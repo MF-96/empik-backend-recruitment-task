ddb3e0e32129d0326ff51735179bf680d8681aca
Marcel <marcelinko96@gmail.com>
2022-04-15 14:12:55 +0200

    Initial commit

---
+++ src/main/java/pl/empik/demo/EmpikBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.empik.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpikBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpikBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/empik/demo/EmpikBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.empik.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpikBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
