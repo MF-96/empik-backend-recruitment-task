dba8fa131927a40ce8576e5b4e7c0683c04adc6d
Marcel <marcelinko96@gmail.com>
2022-04-15 15:49:27 +0200

    Fix GithubUser fields names

--- src/main/java/pl/empik/demo/dto/GithubUser.java
+++ src/main/java/pl/empik/demo/dto/GithubUser.java
@@ -24,46 +24,46 @@ public class GithubUser {
   private String avatarUrl;
 
   @JsonProperty("gravatar_id")
-  private String gravatar_id;
+  private String gravatarId;
 
   @JsonProperty("url")
   private String url;
 
   @JsonProperty("html_url")
-  private String html_url;
+  private String htmlUrl;
 
   @JsonProperty("followers_url")
-  private String followers_url;
+  private String followersUrl;
 
   @JsonProperty("following_url")
-  private String following_url;
+  private String followingUrl;
 
   @JsonProperty("gists_url")
-  private String gists_url;
+  private String gistsUrl;
 
   @JsonProperty("starred_url")
-  private String starred_url;
+  private String starredUrl;
 
   @JsonProperty("subscriptions_url")
-  private String subscriptions_url;
+  private String subscriptionsUrl;
 
   @JsonProperty("organizations_url")
-  private String organizations_url;
+  private String organizationsUrl;
 
   @JsonProperty("repos_url")
-  private String repos_url;
+  private String reposUrl;
 
   @JsonProperty("events_url")
-  private String events_url;
+  private String eventsUrl;
 
   @JsonProperty("received_events_url")
-  private String received_events_url;
+  private String receivedEventsUrl;
 
   @JsonProperty("type")
   private String type;
 
   @JsonProperty("site_admin")
-  private Boolean site_admin;
+  private Boolean siteAdmin;
 
   @JsonProperty("name")
   private String name;
@@ -87,13 +87,13 @@ public class GithubUser {
   private String bio;
 
   @JsonProperty("twitter_username")
-  private String twitter_username;
+  private String twitterUsername;
 
   @JsonProperty("public_repos")
-  private Integer public_repos;
+  private Integer publicRepos;
 
   @JsonProperty("public_gists")
-  private Integer public_gists;
+  private Integer publicGists;
 
   @JsonProperty("followers")
   private Integer followers;
@@ -102,8 +102,8 @@ public class GithubUser {
   private Integer following;
 
   @JsonProperty("created_at")
-  private LocalDateTime created_at;
+  private LocalDateTime createdAt;
 
   @JsonProperty("updated_at")
-  private LocalDateTime updated_at;
+  private LocalDateTime updatedAt;
 }
